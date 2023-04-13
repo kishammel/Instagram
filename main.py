992382fd04534c904f687d9fd99299a2a503e90c
kishammel <50231145+kishammel@users.noreply.github.com>
2023-04-13 10:54:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ password = "zD}GR3X~@U"
 
 
 bot = instaloader.Instaloader()
-bot.login(user=username, passwd=password)
+#bot.login(user=username, passwd=password)
+
 #Profile allocation
 profile = instaloader.Profile.from_username(bot.context, 'kishammel')
+#print(profile.get_posts())
+"""""
+# returns if a proile is private
+private = profile.is_private
 
+print(private)
+
+# returns if a profile is business
+business = profile.is_business
+
+print(business)
+"""""
+""""
 #Profile data
 print("Username: ", profile.username)
 print("User ID: ", profile.userid)
@@ -17,7 +30,7 @@ print("Followers Count: ", profile.followers)
 print("Following Count: ", profile.followees)
 print("Bio: ", profile.biography)
 print("External URL: ", profile.external_url)
-
+"""
 
 """
 #followers
@@ -37,6 +50,31 @@ followers_df.to_csv('followers.csv', index=True)
 #Downloading any public accounts posts
 posts = profile.get_posts()
 for index, post in enumerate(posts, 1):
+    print(post)
     bot.download_post(post, target=f"{profile.username}_{index}")
+
 """
+
+"""""
+posts = profile.get_posts()
+comments_arr = []
+
+for post in posts:
+   print(post.likes)
+"""
+
+
+
+post = instaloader.Post.from_shortcode(bot.context, 'CBAQLopB6d3')
+
+comments_from_loop_including_answers = []
+
+for comment in post.get_comments():
+    comments_from_loop_including_answers.append(comment)
+    for answer in comment.answers:
+        comments_from_loop_including_answers.append(answer)
+
+print(len(comments_from_loop_including_answers))
+print(post.comments)
+
 
